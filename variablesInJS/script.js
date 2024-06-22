7e1d5ca01dbdb063af0626f79c829a188d70fd0a
Samanta <samantawright996@gmail.com>
2024-06-22 12:46:53 +0300

    added js project

---
+++ variablesInJS/script.js
@@ -0,0 +1,15 @@
+let word1 = 'hello '
+console.log(word1)
+let word2 = 'world '
+console.log(word2)
+let word3 = 'from '
+console.log(word3)
+let word4 = 'Samanta!'
+console.log(word4)
+let fulSentence = word1+word2+word3+word4
+console.log(fulSentence )
+// вывод кавычек
+const bigmouth = "Жанна Д'Арк — \"народная героиня Франции.\"";
+console.log (bigmouth)
+
+
