208f631d9a07aeeeb3ceb90c1e9749a5b8ff4701
Samanta <samantawright996@gmail.com>
2024-07-31 21:48:06 +0700

    added operations plus, minus and AC

--- JSCalculator/mainScript.js
+++ JSCalculator/mainScript.js
@@ -1,17 +1,13 @@
 let counter=0;
 document.addEventListener("DOMContentLoaded",function(){
-const oneB = document.getElementById("oneB");
-const twoB = document.getElementById("twoB");
 const plusB = document.getElementById("plusB");
 const equalsB= document.getElementById("equalsB");
 const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
 const result = document.getElementById("result");
 
 
-let res1;
-let res2;
 let res3;
+let operator;
 
 document.addEventListener("keydown", function(event){
     console.log(event.key);
@@ -57,53 +53,34 @@ document.addEventListener("keydown", function(event){
             counter+=1;
         }
             }
-    if(event.key=="+"){
-        if(num1.textContent==0){
-            num1.textContent = result.textContent;   
-        }
-        else{
-            num2.textContent = result.textContent;   
-        }
-        result.textContent = "" ;
-        counter = 0;
-     }
-     if(event.key=="="){
-        result.textContent = Number( num1.textContent ) + Number(num2.textContent);
-     }
-    if(event.key=="-"){
-        if(num1.textContent==0){
-            num1.textContent = result.textContent;   
-        }
-        else{
-            num2.textContent = result.textContent;   
-        }
-        result.textContent = "" ;
-        counter = 0;
-    }
-
-
 });
-
-oneB.addEventListener("click",function(){
-     num1.textContent = oneB.textContent;//изменили содержимое числового поля 1 на содержимое oneB
-     res1 =  num1.textContent;
-});
-twoB.addEventListener("click",function(){
-    num2.textContent = twoB.textContent;
-    res2 =  num2.textContent;
-    });
 plusB.addEventListener("click",function(){
-    console.log(result);
-    res3 =  parseInt(res1)+ parseInt(res2);
-    
-    console.log(res3);
+    num1.textContent = result.textContent;
+    result.textContent="";
+    operator = plusB.textContent;
+   // res3 =  parseInt(res1)+ parseInt(res2);
+   // console.log(res3);
     });
  minusB.addEventListener("click",function(){
-    res3 = parseInt(res1)-parseInt(res2);
+    num1.textContent = result.textContent;
+    result.textContent="";
+    operator = minusB.textContent;
+    //res3 = parseInt(res1)-parseInt(res2);
         });
 equalsB.addEventListener("click",function(){
-    equalsB.textContent;
-    result.textContent = res3;
+    if(num1.textContent!=""){
+        if(operator=="+"){
+            res3 =  parseInt(num1.textContent)+ parseInt(result.textContent);
+        }
+        if(operator=="-"){
+            res3 = parseInt(num1.textContent) - parseInt(result.textContent);
+        }
+        console.log("результат: " + res3);
+        console.log("число 1: " + num1.textContent);
+        console.log("число 2: " + result.textContent);
+        console.log("оператор: " + operator);
+        result.textContent = res3;
+    }
     });
 });
 
@@ -113,4 +90,11 @@ function addBtnValue(value){
     result.textContent +=value;
     counter +=1;
     }
+}
+
+function AC(){
+    result.textContent = "";
+    num1.textContent = "";
+    operator = "";
+    counter = 0;
 }
