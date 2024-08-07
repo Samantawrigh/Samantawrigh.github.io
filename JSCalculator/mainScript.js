4c8cc99652bf3e19860ab4e69e5d9e48fd349a98
Samanta <samantawright996@gmail.com>
2024-08-07 21:52:30 +0700

    We added function makeOpposite and was fixed bug with float numbers

--- JSCalculator/mainScript.js
+++ JSCalculator/mainScript.js
@@ -1,9 +1,13 @@
 let counter=0;
+let isDotExists=
 document.addEventListener("DOMContentLoaded",function(){
+const multiplyB = document.getElementById("multiplyB");
 const plusB = document.getElementById("plusB");
+const minusB = document.getElementById("minusB");
 const equalsB= document.getElementById("equalsB");
 const num1 = document.getElementById("num1");
 const result = document.getElementById("result");
+const divideB = document.getElementById("divideB");
 
 
 let res3;
@@ -54,6 +58,13 @@ document.addEventListener("keydown", function(event){
         }
             }
 });
+
+multiplyB.addEventListener("click",function(){
+    num1.textContent = result.textContent;
+    result.textContent="";
+    operator = multiplyB.textContent;
+    });
+
 plusB.addEventListener("click",function(){
     num1.textContent = result.textContent;
     result.textContent="";
@@ -61,19 +72,30 @@ plusB.addEventListener("click",function(){
    // res3 =  parseInt(res1)+ parseInt(res2);
    // console.log(res3);
     });
- minusB.addEventListener("click",function(){
+minusB.addEventListener("click",function(){
     num1.textContent = result.textContent;
     result.textContent="";
     operator = minusB.textContent;
     //res3 = parseInt(res1)-parseInt(res2);
         });
+divideB.addEventListener("click",function(){
+    num1.textContent = result.textContent;
+    result.textContent="";
+    operator =divideB.textContent;
+     });
 equalsB.addEventListener("click",function(){
     if(num1.textContent!=""){
         if(operator=="+"){
-            res3 =  parseInt(num1.textContent)+ parseInt(result.textContent);
+            res3 =  parseFloat(num1.textContent)+ parseFloat(result.textContent);
         }
         if(operator=="-"){
-            res3 = parseInt(num1.textContent) - parseInt(result.textContent);
+            res3 = parseFloat(num1.textContent) - parseFloat(result.textContent);
+        }
+        if(operator=="X"){
+            res3 = parseFloat(num1.textContent) * parseFloat(result.textContent);
+        }
+        if(operator=="/"){
+            res3 = parseFloat(num1.textContent) / parseFloat(result.textContent);
         }
         console.log("результат: " + res3);
         console.log("число 1: " + num1.textContent);
@@ -86,15 +108,35 @@ equalsB.addEventListener("click",function(){
 
 function addBtnValue(value){
     if(counter<6){
-    console.log(counter);
-    result.textContent +=value;
-    counter +=1;
+        console.log(counter);
+        result.textContent +=value;
+        counter +=1;
     }
 }
+
+function addDot(){
+    if(result.textContent != ""){
+        console.log(result.textContent.indexOf("."));
+        if(result.textContent.indexOf(".")==-1){
+            result.textContent += ".";
+        }
+    }
+}   
 
 function AC(){
     result.textContent = "";
     num1.textContent = "";
     operator = "";
     counter = 0;
+}
+
+function makeOpposite(){
+    if(result.textContent != ""){
+        if(result.textContent.indexOf("-")==-1){
+            result.textContent = "-" + result.textContent;
+        }
+        else{
+            result.textContent=result.textContent.slice(1);
+        }
+    }
 }
