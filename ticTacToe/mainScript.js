ed0f03857b6e3df6f94b902744dfa37d2f25ec9f
Samanta <samantawright996@gmail.com>
2024-11-02 12:30:41 +0300

    frontend developed

---
+++ ticTacToe/mainScript.js
@@ -0,0 +1,21 @@
+undefined
+board = [[0,1,2],
+        [3,4,5],
+        [6,7,8]];
+(3) [Array(3), Array(3), Array(3)]
+for (let i of board) {
+    for (let j of i) {
+      console.log(j);
+    }
+  }
+   0
+   1
+   2
+   3
+   4
+   5
+   6
+   7
+   9
+
+  
