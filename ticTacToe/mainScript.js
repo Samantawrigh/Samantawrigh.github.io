1bbc2ef367c82a25c5523556da775fd2be424baa
Samanta <samantawright996@gmail.com>
2024-11-09 13:29:24 +0300

    .

--- ticTacToe/mainScript.js
+++ ticTacToe/mainScript.js
@@ -1,21 +1,42 @@
-undefined
-board = [[0,1,2],
-        [3,4,5],
-        [6,7,8]];
-(3) [Array(3), Array(3), Array(3)]
-for (let i of board) {
-    for (let j of i) {
-      console.log(j);
+const cells=document.querySelectorAll(".cell");
+let board = [[0,0,0],
+            [0,0,0],
+            [0,0,0]];
+let row;
+let column;
+let isX=true;
+function handleClick(event) {
+    const index = event.target.dataset.index;
+    //строки
+    if(index==0||index==1||index==2){
+        row=0;
     }
-  }
-   0
-   1
-   2
-   3
-   4
-   5
-   6
-   7
-   9
+    if(index==3||index==4||index==5){
+        row=1;
+    }
+    if(index==6||index==7||index==8){
+        row=2;
+    }
+    //столбцы
 
-  
+    if(index==0||index==3||index==6){
+        column=0;
+    }
+    if(index==1||index==4||index==7){
+        column=1;
+    }
+    if(index==2||index==5||index==8){
+        column=2;   
+    }
+    // board[row][column]=1;
+     if(isX){
+        board[row][column]="X";
+        isX=false;
+     }
+     else{
+        board[row][column]="O";
+        isX=true;
+     }
+    console.table(board);
+}
+cells.forEach((cell) => cell.addEventListener("click", handleClick));
