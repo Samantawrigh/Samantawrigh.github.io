44e67fefb3acbb50b73d96cea3ac5d06bd933c2e
Samanta <samantawright996@gmail.com>
2025-01-04 11:51:23 +0300

    ...

--- ticTacToe/mainScript.js
+++ ticTacToe/mainScript.js
@@ -1,7 +1,9 @@
 const cells=document.querySelectorAll(".cell");
+const animationCircleX = document.querySelector(".animCircleX");
+const animationCircleO = document.querySelector(".animCircleO");
 let board = [[0,0,0],
-            [0,0,0],
-            [0,0,0]];
+[0,0,0],
+[0,0,0]];
 let row;
 let column;
 let isX=true;
@@ -18,7 +20,7 @@ function handleClick(event) {
         row=2;
     }
     //столбцы
-
+    
     if(index==0||index==3||index==6){
         column=0;
     }
@@ -30,18 +32,124 @@ function handleClick(event) {
     }
     // board[row][column]=1;
     const cell=cells[index];
-     if(isX){
+    if(isX){
         board[row][column]="X";
         isX=false;
         const divX = cell.querySelector(".X");
         divX.classList.remove("disable");
+        animationCircleX.classList.remove("disable");
+        animationCircleO.classList.add("disable");
      }
      else{
         board[row][column]="O";
         isX=true;
         const divO = cell.querySelector(".O");
         divO.classList.remove("disable");
+        animationCircleO.classList.remove("disable");
+        animationCircleX.classList.add("disable");
      }
+     
     console.table(board);
+
+    if(boardChecking()){
+     console.log("ПОБЕДА!")
+    }
+
 }
 cells.forEach((cell) => cell.addEventListener("click", handleClick));
+
+function boardChecking(){
+    let firstSymbol;
+    let result = 0;
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board.length;j++){
+           if(board[i][j]===0){
+            result = 0;
+                break;
+           }
+           else{
+                if(j == 0){
+                    firstSymbol=board[i][j];
+                }
+                if(firstSymbol!=board[i][j]){
+                    result = 0;
+                    break; 
+                }
+                else{
+                    result++;
+                }
+           }
+           if(result == 3){
+            return true;
+           }
+        }
+    }
+    result = 0;
+    for(let i = 0; i < board.length;i++){
+        for(let j = 0; j < board.length; j++){
+           if(board[j][i]===0){
+            result = 0;
+                break;
+           }
+           else{
+                if(i == 0){
+                    firstSymbol=board[j][i];
+                }
+                if(firstSymbol!=board[j][i]){
+                    result = 0;
+                    break; 
+                }
+                else{
+                    result++;
+                }
+           }
+           if(result == 3){
+            return true;
+           }
+        }
+    }
+    result = 0;
+    for(let i = 0; i < board.length;i++){
+        if(board[i][i]===0){
+            result = 0;
+                break;
+           }
+           else{
+                if(i == 0){
+                    firstSymbol=board[i][i];
+                }
+                if(firstSymbol!=board[i][i]){
+                    result = 0;
+                    break; 
+                }
+                else{
+                    result++;
+                }
+           }
+           if(result == 3){
+            return true;
+           }
+    }
+    for(let i = 0; i < board.length;i++){
+        if(board[i][board.length-i-1]===0){
+            result = 0;
+                break;
+           }
+           else{
+                if(i == 0){
+                    firstSymbol=board[i][board.length-i-1];
+                }
+                if(firstSymbol!=board[i][board.length-i-1]){
+                    result = 0;
+                    break; 
+                }
+                else{
+                    result++;
+                }
+           }
+           if(result == 3){
+            return true;
+           }
+    }
+    return false;
+}
